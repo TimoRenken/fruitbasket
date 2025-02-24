ff48634843e72b1755c51e43e9fdf20c74860932
TimoRenken <timorenken@outlook.de>
2025-02-24 11:18:48 +0100

    added HTML and SCSS from my teacher

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
 export class FruitlistComponent {
+
   fruitlist = [
     {
       name: "Apfel",
