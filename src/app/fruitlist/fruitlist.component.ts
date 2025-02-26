2e83b9f7df82fd264d89c3b9f5b5e7f7fd6fa0b0
TimoRenken <timorenken@outlook.de>
2025-02-26 10:57:05 +0100

    first steps with ngStyle + ternaryOperator

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -10,6 +10,10 @@ import { Component } from '@angular/core';
 })
 export class FruitlistComponent {
 
+  fontColorGood = 'green'
+  fontColorBad = 'red'
+
+
   fruitlist = [
     {
       name: "Apfel",
