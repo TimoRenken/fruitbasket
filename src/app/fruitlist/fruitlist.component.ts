3db8760c66179030ce8ef8bc2e0f79a788d0f012
TimoRenken <timorenken@outlook.de>
2025-03-01 10:30:45 +0100

    added singlefruit component

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SinglefruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
@@ -12,7 +13,6 @@ export class FruitlistComponent {
 
   fontColorGood = 'green'
   fontColorBad = 'red'
-
 
   fruitlist = [
     {
