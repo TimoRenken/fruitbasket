66e0221180ccecd44edb35de68ce017d724be548
TimoRenken <timorenken@outlook.de>
2025-03-02 09:26:01 +0100

    learned about communication from child component to parent component

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -64,4 +64,9 @@ export class FruitlistComponent {
       reviews:[{name: "Kevin W.", text: "Ganz gut in der Regel!"},{name: "Oliver J.", text: "Absoluter Favorit beim Frühstück"}],
     },
   ]
+
+  nameLog(name:string){
+    console.log(name);
+  }
+
 }
